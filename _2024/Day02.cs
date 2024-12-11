51ab1e39bcd0d7f8ea9bb51882c84f0dd64d3b8c
Ben Davidson <ginge6000@hotmail.com>
2024-12-11 13:58:33 +0000

    Solve Day 2

--- _2024/Day02.cs
+++ _2024/Day02.cs
@@ -16,23 +16,51 @@ namespace AdventOfCode._2024
             {
                 var lineArray = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x)).ToList();
 
-                List<int> report = new List<int>();
-
-                for (int i = 0; i < lineArray.Count -1; i++)
+                if (IsSafe(Report(lineArray)))
                 {
-                    report.Add(lineArray[i+1] - lineArray[i]);
+                    total = total + 1;                   
                 }
-
-                if (partNo == 1)
+                else
                 {
-                    if ((report.Where(x => x > 0).Count() == report.Count
-                        || report.Where(x => x < 0).Count() == report.Count)
-                        && !report.Any(x => Math.Abs(x) > 3)
-                        && !report.Any(x => Math.Abs(x) < 1))
+                    if(partNo == 2)
                     {
-                        total = total + 1;
+                        for (int i = 0; i < lineArray.Count; i++)
+                        {
+                            if(IsSafe(Report(lineArray.Where((x, y) => y != i).ToList())))
+                            { 
+                                total = total + 1;
+                                break;
+                            }
+                        }
                     }
                 }
+            }
+        }
+
+        private List<int> Report(List<int> lineArray)
+        {
+            List<int> report = new List<int>();
+
+            for (int i = 0; i < lineArray.Count - 1; i++)
+            {
+                report.Add(lineArray[i + 1] - lineArray[i]);
+            }
+
+            return report;
+        }
+        
+        private bool IsSafe(List<int> report)
+        {
+            if ((report.Where(x => x > 0).Count() == report.Count
+                    || report.Where(x => x < 0).Count() == report.Count)
+                    && !report.Any(x => Math.Abs(x) > 3)
+                    && !report.Any(x => Math.Abs(x) < 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
