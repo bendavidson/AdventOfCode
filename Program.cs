e57dbee6164e072d6def431190ebda7b3467bc78
Ben Davidson <ginge6000@hotmail.com>
2024-12-11 11:44:33 +0000

    Create puzzle day objects dynamically based on year & day

--- Program.cs
+++ Program.cs
@@ -6,55 +6,25 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the year you are solving");
+            var year = Console.ReadLine();
             Console.WriteLine("Enter the day you are solving");
             var dayNo = Console.ReadLine();
-            Console.WriteLine("You are solving puzzles from day " +  dayNo);
+            Console.WriteLine("You are solving puzzles from day " + dayNo + ", " + year + ".");
             IPuzzle puzzle;
             
-            switch (dayNo)
+            var dayPuzzle = Type.GetType("AdventOfCode._" + year + ".Day" + dayNo.PadLeft(2,'0'));
+
+            if (dayPuzzle != null)
             {
-                case "1":
-                    puzzle = new Day01(); break;
-                case "2":
-                    puzzle = new Day02(); break;
-                case "3":
-                    puzzle = new Day03(); break;
-                case "4":
-                    puzzle = new Day04(); break;
-                case "5":
-                    puzzle = new Day05(); break;
-                case "6":
-                    puzzle = new Day06(); break;
-                case "7":
-                    puzzle = new Day07(); break;
-                case "8":
-                    puzzle = new Day08(); break;
-                case "9":
-                    puzzle = new Day09(); break;
-                case "10":
-                    puzzle = new Day10(); break;
-                case "11":
-                    puzzle = new Day11(); break;
-                case "12":
-                    puzzle = new Day12(); break;
-                case "13":
-                    puzzle = new Day13(); break;
-                case "14":
-                    puzzle = new Day14(); break;
-                case "15":
-                    puzzle = new Day15(); break;
-                case "16":
-                    puzzle = new Day16(); break;
-                case "17":
-                    puzzle = new Day17(); break;
-                case "18":
-                    puzzle = new Day18(); break;
-                case "19":
-                    puzzle = new Day19(); break;
-                default:
-                    Console.WriteLine("No puzzle solution has been created for day " + dayNo);
-                    break;
+                puzzle = Activator.CreateInstance(dayPuzzle) as IPuzzle;
             }
+            else
+            {
+                Console.WriteLine("No puzzle solution has been created for day " + dayNo + ", " + year + ".");
+                Console.ReadLine();
+            }
+            
         }
     }
 }
