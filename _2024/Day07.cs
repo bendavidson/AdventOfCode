6f9f400f1eece0a5c705bd0c38fdde43a560aa98
Ben Davidson <ginge6000@hotmail.com>
2024-12-19 16:49:37 +0000

    Solve Day 7 part 1

---
+++ _2024/Day07.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2024
+{
+    internal class Day07 : DayBase
+    {
+        public Day07() : base("2024", "Day07") { }
+
+        protected override void Solve()
+        {
+            List<(long, int[])> equations = new List<(long, int[])>();
+
+            foreach(var line in lines)
+            {
+                var equationSplit = line.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                equations.Add((Convert.ToInt64(equationSplit[0]), 
+                    equationSplit[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => Convert.ToInt32(x)).ToArray()));
+            }
+
+            foreach(var equation in equations)
+            {
+                if (SolveEquation(equation.Item1, equation.Item2))
+                    total = total + equation.Item1;
+            }
+        }
+
+        private bool SolveEquation(long answer, int[] numbers)
+        {
+            if (numbers.Length > 2)
+            {
+                return (SolveEquation(answer - numbers[numbers.Length - 1], numbers.SkipLast(1).ToArray()) && (answer - numbers[numbers.Length - 1] > 0))
+                    || (SolveEquation(answer / numbers[numbers.Length - 1], numbers.SkipLast(1).ToArray()) && (answer % numbers[numbers.Length - 1] == 0));
+            }
+            else
+            {
+                return (answer == numbers[0] +  numbers[1]) || (answer == numbers[0] * numbers[1]);
+            }
+        }
+    }
+}
